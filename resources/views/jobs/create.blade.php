bea6e27db17cd197fb2125058660419b20d4c356
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-03-04 14:37:15 -0800

    create jobs views and  controllers

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Job</title>
+</head>
+
+<body>
+    <h1>Create New Job</h1>
+    <form action="/jobs" method="POST">
+    @csrf
+    <input type="text" name="title" placeholder="title">
+    <input type="text" name="description" placeholder="description">
+    <button
+        type="submit"
+        class="btn btn-outline-primary"
+    >
+        Button
+    </button>
+    
+    </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+
+// Jobs routes
+Route::resource('jobs', JobController::class);
