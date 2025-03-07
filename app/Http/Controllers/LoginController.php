bf978de8768d3a82ade7ac7682dd8ac96602b75a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-03-07 09:48:18 -0800

    Login user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 
@@ -12,5 +13,38 @@ class LoginController extends Controller {
     // @route GET /login
     public function login(): View {
         return view('auth.login');
+    }
+
+    // @desc   Authenticate user
+    // @route   POST /login
+    public function authenticate(Request $request): RedirectResponse {
+        $credentials = $request->validate([
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string',
+        ]);
+
+        // Attempt to authenticate user
+        if (Auth::attempt($credentials)) {
+            // Regenerate the session to prevent fixation attacks
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'))->with('success', 'You are now logged in!');
+        }
+
+        // If auth fails, redirect back with error
+        return back()->withErrors([
+            'email' => 'The credentials you entered do not match our records'
+        ])->onlyInput('email');
+    }
+
+    // @desc    Logout user
+    // @route   POST /logout
+    public function logout(Request $request): RedirectResponse {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
