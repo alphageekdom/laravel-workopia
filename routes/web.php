61df749079e7eb5ad55f2cb1f8212f9e7e16af02
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-03-07 10:57:35 -0800

    Guest middleware and groups

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,20 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('jobs', JobController::class)->middleware('auth')->only(['create', 'edit', 'update', 'destroy']);
 Route::resource('jobs', JobController::class)->except(['create', 'edit', 'update', 'destroy']);
 
+// Middleware protected routes
+Route::middleware('guest')->group(function () {
+    // Register routes
+    Route::get('/register', [RegisterController::class, 'register'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+
+    // Login routes
+    Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+});
 
 // Jobs routes
 Route::resource('jobs', JobController::class);
-
-// Register routes
-Route::get('/register', [RegisterController::class, 'register'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
-// Login routes
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 
 // Logout routes
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
